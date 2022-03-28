7ef28fc305ba63fce6a1d80ae64c846687d6d8b8
fabien <fabien.andrian@gmail.com>
2022-03-28 21:31:55 +0200

    bughunt : admin can edit bills and into different containers without problem

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -85,26 +85,26 @@ export default class {
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
 
-  handleEditTicket(e, bill, bills) {
-    if (this.counter === undefined || this.id !== bill.id) this.counter = 0
-    if (this.id === undefined || this.id !== bill.id) this.id = bill.id
-    if (this.counter % 2 === 0) {
-      bills.forEach(b => {
-        $(`#open-bill${b.id}`).css({ background: '#0D5AE5' })
+  handleEditTicket(e, bill, bills, containerIndex) {
+
+    if (this.billContainerIsActive[containerIndex]) {
+      bills.forEach(bill => {
+        $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
       })
       $(`#open-bill${bill.id}`).css({ background: '#2A2B35' })
       $('.dashboard-right-container div').html(DashboardFormUI(bill))
       $('.vertical-navbar').css({ height: '150vh' })
-      this.counter ++
-    } else {
+    } 
+    
+    else {
       $(`#open-bill${bill.id}`).css({ background: '#0D5AE5' })
 
       $('.dashboard-right-container div').html(`
         <div id="big-billed-icon" data-testid="big-billed-icon"> ${BigBilledIcon} </div>
       `)
       $('.vertical-navbar').css({ height: '120vh' })
-      this.counter ++
     }
+
     $('#icon-eye-d').click(this.handleClickIconEye)
     $('#btn-accept-bill').click((e) => this.handleAcceptSubmit(e, bill))
     $('#btn-refuse-bill').click((e) => this.handleRefuseSubmit(e, bill))
@@ -130,23 +130,28 @@ export default class {
     this.onNavigate(ROUTES_PATH['Dashboard'])
   }
 
-  handleShowTickets(e, bills, index) {
-    if (this.counter === undefined || this.index !== index) this.counter = 0
-    if (this.index === undefined || this.index !== index) this.index = index
-    if (this.counter % 2 === 0) {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
-      this.counter ++
-    } else {
-      $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
-      $(`#status-bills-container${this.index}`)
-        .html("")
-      this.counter ++
+  handleShowTickets(e, bills, containerIndex) {
+
+    if (this.billContainerIsActive === undefined) {
+      this.billContainerIsActive = {}
     }
 
+    if (!this.billContainerIsActive[containerIndex]) {
+      $(`#arrow-icon${containerIndex}`).css({ transform: 'rotate(0deg)'})
+      $(`#status-bills-container${containerIndex}`)
+        .html(cards(filteredBills(bills, getStatus(containerIndex))))
+        this.billContainerIsActive[containerIndex] = true
+    }
+    
+    else {
+      $(`#arrow-icon${containerIndex}`).css({ transform: 'rotate(90deg)'})
+      $(`#status-bills-container${containerIndex}`)
+        .html("")
+        this.billContainerIsActive[containerIndex] = false
+      }
+
     bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills, containerIndex))
     })
 
     return bills
